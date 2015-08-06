318f5f28244d0d07f8d9a348920d1d400b2033cb
Zacaria <zc@42.am>
2015-08-06 13:55:38 +0200

    removed cache for jade

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.set('view engine', 'html');
+app.set('view cache', false);
 app.set('views', __dirname + '/public/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
